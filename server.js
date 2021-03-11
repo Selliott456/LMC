022dda713ac50b6a956f789fb2647b61c48ae952
Sherryll Elliott <sherryllelliott@gmail.com>
2021-03-11 12:26:18 +0000

    basic express server set up with logger and port

---
+++ server.js
@@ -0,0 +1,19 @@
+//import is still fairly new for node. Require is fully stable
+
+const express = require('express')
+
+//express is a function that helps you create a webserver
+const expressServer = express()
+
+//logging
+//use takes a function as an argument
+//request object
+//response object
+//next - tells express to move on
+expressServer.use((req, res, next)=> {
+  console.log(`incoming request, ${req.method} to ${req.url}`)
+  next()
+})
+
+//port assigned for express to listen to
+expressServer.listen(5000)
